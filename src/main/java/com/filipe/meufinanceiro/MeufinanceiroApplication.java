25e33b7f81db43094276894b58ff4e65cf9249f0
Filipe <108732180+ShoemixDev@users.noreply.github.com>
2025-03-09 11:31:46 -0300

    Initial commit

---
+++ src/main/java/com/filipe/meufinanceiro/MeufinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.filipe.meufinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeufinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeufinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filipe/meufinanceiro/MeufinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipe.meufinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeufinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
